662915d302df207c31328359f546062597ad8dcb
雷宇 <amon.lei@gmail.com>
2014-07-12 19:03:37 +0800

    增加插件源码

---
+++ docs/phonegap_plugins/cordovarduino-master/www/serial.js
@@ -0,0 +1,58 @@
+var serial = {
+    requestPermission: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'requestPermission',
+            []
+        );
+    },
+    open: function(opts, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'openSerial',
+            [{'opts': opts}]
+        );
+    },
+    write: function(data, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'writeSerial',
+            [{'data': data}]
+        );
+    },
+    read: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'readSerial',
+            []
+        );
+    },
+    close: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'closeSerial',
+            []
+        );
+    },
+    registerReadCallback: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'registerReadCallback',
+            []
+        );
+    }
+    
+};
+module.exports = serial;
